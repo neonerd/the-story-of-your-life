1d6e7f5eb052a8b2b2d792efdfa6f54776ddb347
Andrej Sykora <as@andrejsykora.com>
2019-03-26 10:05:30 +0100

    feat: filter working

--- src/client/index.ts
+++ src/client/index.ts
@@ -32,26 +32,33 @@ document.body.appendChild(app.view)
 
 // The stage is the root container that will hold everything in our scene
 const stage = new PIXI.Container()
+
+
+//Get shader code as a string
+var uniforms: any = {};
+uniforms.time = {type: '1f',value: 0};
+//Create our Pixi filter using our custom shader code
+var shaderCode = document.getElementById("shader-film-grain").innerHTML
+var simpleShader = new PIXI.Filter('', shaderCode, uniforms);
+
+// Create the photo
 const photo = PIXI.Sprite.fromImage("/res/stills_nos/tumblr_mw87reA3de1sfie3io1_1280.jpg")
 photo.x = monitorSettings.w / 2;
 photo.y = monitorSettings.h / 2;
 // Make sure the center point of the image is at its center, instead of the default top left
 photo.anchor.set(0.5);
+//Apply it to our object
+photo.filters = [simpleShader]
 // Add it to the screen
 stage.addChild(photo);
 
-//Get shader code as a string
-var shaderCode = document.getElementById("shader-film-grain").innerHTML
-//Create our Pixi filter using our custom shader code
-var simpleShader = new PIXI.Filter('',shaderCode);
-//Apply it to our object
-photo.filters = [simpleShader]
+const startTime = Date.now()
 
 function animate() {
     // start the timer for the next animation loop
     requestAnimationFrame(animate);
     // do time delta
-    (simpleShader.uniforms as any).time.value = (simpleShader.uniforms as any).time.value+1
+    (simpleShader.uniforms as any).time = Date.now() - startTime
     // this is the main render call that makes pixi draw your container and its children.
     app.renderer.render(stage);
 }
